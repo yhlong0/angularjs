fb3c4a2bf3799637522fb4d9567c7a0cbe9ffbd8
yhlong0 <yhlong2@gmail.com>
2017-04-02 23:47:52 -0500

    add github services

--- script.js
+++ script.js
@@ -2,17 +2,15 @@
 
 	var app = angular.module("githubViewer", []);
 
-	var MainController = function(
-		$scope, $http, $interval, 
-		$log, $anchorScroll, $location){
-		var onUserComplete = function(response){
-			$scope.user = response.data;
-			$http.get($scope.user.repos_url)
+	var MainController = function($scope, github, $interval, $log, $anchorScroll, $location){
+		var onUserComplete = function(data){
+			$scope.user = data;
+			github.getRepos($scope.user)
 				 .then(onRepos, onError);
 		};
 
-		var onRepos = function(response){
-			$scope.repos = response.data;
+		var onRepos = function(data){
+			$scope.repos = data;
 			$location.hash("userDetails");
 			$anchorScroll();
 		};
@@ -23,8 +21,7 @@
 
 		$scope.search = function(username){
 			$log.info("Searching for " + username);
-			$http.get("https://api.github.com/users/" + username)
-			 .then(onUserComplete, onError);
+			github.getUser(username).then(onUserComplete, onError);
 			 if(countdownInterval){
 			 	$interval.cancel(countdownInterval);
 			 }
